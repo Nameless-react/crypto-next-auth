76669533f69fafc6ffc60109a63653d6cf990fec
joel <joel33960@gmail.com>
2022-04-22 18:13:33 -0600

    deploy

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
-export default dev ? "http://localhost:3000" : "https://startling-fairy-bf17eb.netlify.app"; 
+export default dev ? "https://startling-fairy-bf17eb.netlify.app" : "http://localhost:3000"; 
 // export default "http://localhost:3000"
