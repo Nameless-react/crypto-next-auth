aa59bb6d6b2e65b7fc526030110cc45eab27f892
joel <joel33960@gmail.com>
2022-04-24 14:15:25 -0600

    deploy-vercel

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
-export default dev ? "http://localhost:3000" : process.env.VERCE_URL;
+export default dev ? "http://localhost:3000" : process.env.NEXT_PUBLIC_VERCEL_URL;
 // export default "https://startling-fairy-bf17eb.netlify.app"
 // export default "http://localhost:3000"
 // export default process.env.NEXT_PUBLIC_API_URL
