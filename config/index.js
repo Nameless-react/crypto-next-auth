76be23adb4dfc2bcaedc9f41d06e0664c9cf769a
joel <joel33960@gmail.com>
2022-04-24 12:59:04 -0600

    deploy-vercel

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
-export default dev ? "http://localhost:3000" : "https://crypto-next-auth.vercel.app";
+// export default dev ? "http://localhost:3000" : "https://crypto-next-auth.vercel.app";
 // export default "https://startling-fairy-bf17eb.netlify.app"
 // export default "http://localhost:3000"
+export default process.env.NEXT_PUBLIC_API_URL
