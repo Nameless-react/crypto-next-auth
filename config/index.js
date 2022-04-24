356eaf1e0516ed48ed6ed9695b43e4e61b72d09e
joel <joel33960@gmail.com>
2022-04-24 17:59:46 -0600

    deploy--

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== "production";
-export default dev ? "http://localhost:3000" : process.env.VERCEL_URL;
+export default dev ? "http://localhost:3000" : "https://crypto-next-auth-1.vercel.app";
