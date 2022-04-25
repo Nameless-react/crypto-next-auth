92d5093936b26b2906b372b3c94526d08b529bcd
joel <joel33960@gmail.com>
2022-04-24 18:25:25 -0600

    deploy---

--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -1,10 +1,10 @@
 import url from '../../../config/index';
 export default async function handler(req, res) {
-    const rest = await fetch(`${url}/api/articles`);
+    const rest = await fetch(`https://jsonplaceholder.typicode.com/posts`);
     const data = await rest.json(); 
     const { id } = req.query;
     const filtered = data.filter(element => element.id === parseInt(id));
-    if(filtered.length > 0) {
+    if (filtered.length > 0) {
         res.status(200).json(filtered[0]); 
     } else {
         res.status(404).json({
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,42 +1,42 @@
-// import { useRouter } from "next/router"
-// import style from "../../../styles/Home.module.css"
-// import url from "../../../config/index";
-// import Meta from "../../../components/Head";
-// export default function Article(props) {
-//     const router = useRouter()
-//     const {id}  = router.query
-//     return (
-//         <>
-//             <Meta  title={props.article.title}/>
-//             <h1 className={style.id}>{props.article.body ? `This is the article ${id}` : "Article not found"}</h1>
-//             <p className={style.content}>{props.article.body}</p>
-//         </>
-//     )
-// }
+import { useRouter } from "next/router"
+import style from "../../../styles/Home.module.css"
+import url from "../../../config/index";
+import Meta from "../../../components/Head";
+export default function Article(props) {
+    const router = useRouter()
+    const {id}  = router.query
+    return (
+        <>
+            <Meta  title={props.article.title}/>
+            <h1 className={style.id}>{props.article.body ? `This is the article ${id}` : "Article not found"}</h1>
+            <p className={style.content}>{props.article.body}</p>
+        </>
+    )
+}
 
 
-// export const getStaticProps = async (context) => {
-//     const res = await fetch(`${url}/api/articles/${context.params.id}`);
-//     const article = await res.json();
-//     return {
-//         props: {
-//             article
-//         }
-//     }
-// }
+export const getStaticProps = async (context) => {
+    const res = await fetch(`${url}/api/articles/${context.params.id}`);
+    const article = await res.json();
+    return {
+        props: {
+            article
+        }
+    }
+}
 
-// export const getStaticPaths = async () => {
-//     const res = await fetch(`${url}/api/articles`);
-//     const article = await res.json();
-//     const ids = article.map(article => article.id)
-//     const paths = ids.map(id => ({
-//         params: {
-//             id: id.toString()
-//         }
-//     })
-//     );
-//     return {
-//         paths,
-//         fallback: false
-//     }
-// }
+export const getStaticPaths = async () => {
+    const res = await fetch(`${url}/api/articles`);
+    const article = await res.json();
+    const ids = article.map(article => article.id)
+    const paths = ids.map(id => ({
+        params: {
+            id: id.toString()
+        }
+    })
+    );
+    return {
+        paths,
+        fallback: false
+    }
+}
