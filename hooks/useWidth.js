f70114fb142ee5bba98c338e760bcc754a8dc4a0
joel <joel33960@gmail.com>
2022-04-27 20:09:00 -0600

    optimize

--- hooks/useWidth.js
+++ hooks/useWidth.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export default function useWidth() {
     const isServer = typeof window !== "undefined";
-    const [width, setWidth] = useState(isServer && window.innerWidth);
+    const [width, setWidth] = useState(0);
 
     useEffect(() => {
         const resizeHandler = () => {
