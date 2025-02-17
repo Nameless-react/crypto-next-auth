08b862cd049ac89c007abf5ae55b9401346d2c6f
joel <joel33960@gmail.com>
2025-02-16 22:42:59 -0600

    migración parcial con react-query de tanstack

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { signOut, useSession } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightToBracket, faPenToSquare, faFloppyDisk, faXmark } from "@fortawesome/free-solid-svg-icons";
